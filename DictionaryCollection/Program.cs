896c5b45ac2fe9f93292560277ac907f41d4f342
Abraham Ayepah-Mensah <abraham@hubtel.com>
2020-05-14 17:27:56 +0000

    loop manipulation

---
+++ DictionaryCollection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using TopTenPops;
+
+namespace DictionaryCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string filePath = "Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+            var countries = reader.ReadAllCountries();
+
+            Console.WriteLine("Which country code do you want to look up");
+            var userInput = Console.ReadLine();
+
+            bool gotCountry = countries.TryGetValue(userInput, out Country country);
+            if (!gotCountry)
+            {
+                Console.WriteLine($"Sorry, there is no country with the code, {userInput}");
+            }
+            else
+            {
+                Console.WriteLine($"{country.Name} has a population {PopulationFormatter.FormatPopulation(country.Population)}");
+            }
+        }
+    }
+}
--- ListCollection/Program.cs
+++ ListCollection/Program.cs
@@ -12,14 +12,34 @@ namespace DaysOfWeek
             CsvReader reader = new CsvReader(filePath);
 
             List<Country> countries = reader.ReadAllCountries();
-            var lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
-            int lilliputIndex = countries.FindIndex(x=> x.Population < 2_000_000);
-            countries.Insert(lilliputIndex, lilliput);
-            countries.RemoveAt(lilliputIndex);
-
-            foreach (var country in countries)
+            
+            Console.Write("Enter the no. of countries to display> ");
+            bool inputIsInt = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsInt || userInput <= 0)
             {
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+                Console.WriteLine("You must type a positive number. Exiting...");
+                return;
+            }
+
+            int maxToDisplay = userInput;
+
+
+            // foreach (var country in countries)
+            // {
+            //     Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            // }
+
+            for (int i = 0; i < countries.Count; i++)
+            {
+                if (i > 0 && (i % maxToDisplay == 0))
+                {
+                    Console.WriteLine("Hit return to continue, anything else to quit");
+                    if(Console.ReadLine() != "")
+                        break;
+                }
+                var country = countries[i];
+                Console.WriteLine($"{i+1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+
             }
 
         }
